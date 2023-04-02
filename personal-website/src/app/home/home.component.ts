140e7f58a5f0012782e3827a8040c0f191475673
Sascha Pflittner <s.pflittner@gmail.com>
2023-04-02 18:41:59 +0200

    Lebenslauf integriert

--- personal-website/src/app/home/home.component.ts
+++ personal-website/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { faArrowAltCircleRight, faBullseye, faFutbolBall, faGuitar, faHouse } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-
+  faFutbol = faFutbolBall;
+  faGuitar = faGuitar;
+  faDart = faBullseye;
 }
